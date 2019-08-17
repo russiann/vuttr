84bda7dbd135e8a369738ec6f4c725d84cf7ce5b
Russian <russian@MacBook-Pro-8.local>
2019-08-17 17:04:23 -0300

    feat(components): button component

---
+++ src/common/components/button/button.component.js
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  color: ${p => p.theme.textColor};
+  background-color: ${p => p.theme.bgColor};
+  text-align: center;
+  text-transform: uppercase;
+  padding: 10px 50px;
+  border: none;
+  border-radius: 30px;
+  outline: none;
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.9;
+  }
+`;
+
+Button.defaultProps = {
+  theme: {
+    textColor: '#fff',
+    bgColor: '#ff6007'
+  }
+};
+
+export default Button;
---
+++ src/common/components/button/button.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import {storiesOf} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+
+import Button from './button.component';
+
+storiesOf('Button', module).add('with text', () => (
+  <Button onClick={action('clicked')}>Hello Button</Button>
+));
